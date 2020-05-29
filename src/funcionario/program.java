66528b627bfc39248e565e0105a0dab6d2857e45
Igor Machado <igorhms98@gmail.com>
2020-05-29 19:18:22 -0300

    Leitura dos dados

--- src/funcionario/program.java
+++ src/funcionario/program.java
@@ -1,10 +1,43 @@
 package funcionario;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		int horaTrabalhadas;
+		double valorHora;
+		char repetir;
+		
+		do {
+		System.out.print("Nome: ");
+		nome = sc.nextLine();
+		System.out.print("Horas trabalhadas: ");
+		horaTrabalhadas = sc.nextInt();
+		while (horaTrabalhadas < 0){
+			System.out.println("O valor deve ser positivo. Tente novamente.");
+			horaTrabalhadas = sc.nextInt();
+		}
+		System.out.print("Valor por hora: ");
+		valorHora = sc.nextDouble();
+		while (valorHora < 0){
+			System.out.println("O valor deve ser positivo. Tente novamente.");
+			valorHora = sc.nextInt();
+		}
+		System.out.print("Digitar outro (S/N)? ");
+		repetir = sc.next().charAt(0);
+		sc.nextLine();
+		} while (repetir == 'S' || repetir == 's');
+		
+		
+	
+		sc.close();
 	}
 
 }
