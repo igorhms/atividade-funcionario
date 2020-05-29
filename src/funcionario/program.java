868083ed8a9d4c8cfe0e257d5d51340050a940dc
Igor Machado <igorhms98@gmail.com>
2020-05-29 19:34:30 -0300

    Menu repetindo

--- src/funcionario/program.java
+++ src/funcionario/program.java
@@ -9,34 +9,43 @@ public class program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		String nome;
-		int horaTrabalhadas;
+		int horaTrabalhadas, opcao;
 		double valorHora;
-		char repetir;
-		
-		do {
-		System.out.print("Nome: ");
-		nome = sc.nextLine();
-		System.out.print("Horas trabalhadas: ");
-		horaTrabalhadas = sc.nextInt();
-		while (horaTrabalhadas < 0){
-			System.out.println("O valor deve ser positivo. Tente novamente.");
+		char repetir = 0;
+
+		while (repetir != 'n' && repetir != 'N') {
+			System.out.print("Nome: ");
+			nome = sc.next();
+			System.out.print("Horas trabalhadas: ");
 			horaTrabalhadas = sc.nextInt();
+			while (horaTrabalhadas < 0) {
+				System.out.println("O valor deve ser positivo. Tente novamente.");
+				horaTrabalhadas = sc.nextInt();
+			}
+			System.out.print("Valor por hora: ");
+			valorHora = sc.nextDouble();
+			while (valorHora < 0) {
+				System.out.println("O valor deve ser positivo. Tente novamente.");
+				valorHora = sc.nextInt();
+			}
+			System.out.print("Digitar outro (S/N)? ");
+			repetir = sc.next().charAt(0);
 		}
-		System.out.print("Valor por hora: ");
-		valorHora = sc.nextDouble();
-		while (valorHora < 0){
-			System.out.println("O valor deve ser positivo. Tente novamente.");
-			valorHora = sc.nextInt();
-		}
-		System.out.print("Digitar outro (S/N)? ");
-		repetir = sc.next().charAt(0);
-		sc.nextLine();
-		} while (repetir == 'S' || repetir == 's');
-		
-		
-	
+
+		do {
+		System.out.println();
+		System.out.println("MENU");
+		System.out.println("1 - Total de horas trabalhadas");
+		System.out.println("2 - Custo total");
+		System.out.println("3 - Nome da pessoa que ganhou mais");
+		System.out.println("4 - Sair");
+		System.out.print("Digite uma opção: ");
+		opcao = sc.nextInt();
+
+		} while (opcao != 4);
+
 		sc.close();
 	}
 
